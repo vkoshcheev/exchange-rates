f285f33ec19725488cfb9072f25505cc7149ce62
vkoshcheev <ltsix2@gmail.com>
2025-01-20 09:56:30 +0300

    IntroductionSection - added source code link

--- src/sections/IntroductionSection/IntroductionSection.tsx
+++ src/sections/IntroductionSection/IntroductionSection.tsx
@@ -6,6 +6,14 @@ const IntroductionSection = () => {
     <div className="section">
       <p className="section__title">Данный сайт посвящён обменным курсам валют стран СНГ.</p>
 
+      <div className="source-code">
+        <span className="hide-below-720">Исходный код:&nbsp;</span>
+        <a href="https://github.com/vkoshcheev/exchange-rates" target="_blank">
+          <span className="hide-below-720">https://github.com/vkoshcheev/exchange-rates</span>
+          <span className="show-below-720">Исходный код</span>
+        </a>
+      </div>
+
       <p>
         Содружество Независимых Государств (СНГ) — международная организация, призванная регулировать
         отношения сотрудничества между некоторыми государствами, входившими ранее в состав СССР. Основано 8
